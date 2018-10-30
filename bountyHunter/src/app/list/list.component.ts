d0586d1d37992e546adf651acb46b226f03e4894
Maxime Gypteau <maxime.gypteau@gmail.com>
2018-10-30 14:32:10 +0100

    Réalisation liste de bonbon à partir de Mock

--- bountyHunter/src/app/list/list.component.ts
+++ bountyHunter/src/app/list/list.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  public candiesLists:any[]=[
+    {
+      "name":"Haribo",
+      "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmUtYXec86sf9Kfjqor140LELj5edM8EzANq8poCrvLt7LpnjL"
+    },
+    {
+      "name":"M&Ms",
+      "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmUtYXec86sf9Kfjqor140LELj5edM8EzANq8poCrvLt7LpnjL"
+    },
+    {
+      "name":"Tagada",
+      "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmUtYXec86sf9Kfjqor140LELj5edM8EzANq8poCrvLt7LpnjL"
+    },
+    {
+      "name":"Lolilo",
+      "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmUtYXec86sf9Kfjqor140LELj5edM8EzANq8poCrvLt7LpnjL"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
