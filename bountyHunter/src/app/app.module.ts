222f0258686724f2989ab6b85010b149b42fba8f
DrGonzi <thomas.gonzi440@gmail.com>
2018-10-30 15:25:24 +0100

    Service InfoBonbon opé

--- bountyHunter/src/app/app.module.ts
+++ bountyHunter/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
+import { CandyInfoComponent } from './candy-info/candy-info.component';
+import { InfoBonbonService } from './info-bonbon.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandyInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [InfoBonbonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
