6f36f3830d955694484c719d3e2881bb94ebb0ff
Maxime Gypteau <maxime.gypteau@gmail.com>
2018-10-30 15:06:51 +0100

    Commit tempo

--- bountyHunter/src/app/candy/candy.component.ts
+++ bountyHunter/src/app/candy/candy.component.ts
@@ -10,7 +10,6 @@ export class CandyComponent implements OnInit {
   @Input() name:string = "Haribo";
   @Input() img:string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmUtYXec86sf9Kfjqor140LELj5edM8EzANq8poCrvLt7LpnjL";
 
-
   constructor() { }
 
   ngOnInit() {
