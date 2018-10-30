5b4e6ff6509d6575095295e056ca505ada80117f
Gaerdy <gaerdy@gmail.com>
2018-10-30 12:15:00 +0100

    création du composant candy

--- bountyHunter/src/app/app.module.ts
+++ bountyHunter/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CandyComponent } from './candy/candy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandyComponent
   ],
   imports: [
     BrowserModule
