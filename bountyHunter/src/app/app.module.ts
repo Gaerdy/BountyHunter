e521d057d2c06799e66d7ed3b157bdc6f7f59080
etienne rabin <e.rabin05@gmail.com>
2018-10-30 10:58:55 +0100

    Response start

--- bountyHunter/src/app/app.module.ts
+++ bountyHunter/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResponseComponent } from './response/response.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResponseComponent
   ],
   imports: [
     BrowserModule
