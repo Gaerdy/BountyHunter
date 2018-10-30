a8806013c8e2e7c3608c0b9b3c5d3f4e1c7e9595
Maxime Gypteau <maxime.gypteau@gmail.com>
2018-10-30 14:01:26 +0100

    Composant unitaire bonbon

--- bountyHunter/src/app/app.module.ts
+++ bountyHunter/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
 import { CandyComponent } from './candy/candy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
     CandyComponent
   ],
   imports: [
--- bountyHunter/src/app/candy/candy.component.ts
+++ bountyHunter/src/app/candy/candy.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandyComponent implements OnInit {
 
+  public candy:any = {
+    "name":"Haribo",
+    "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5mqg9cwm99ab_iR96QUpIXjr7BlB_fEHZ8EZOKj20SLzMkp9hyw"
+  }
+
   constructor() { }
 
   ngOnInit() {
